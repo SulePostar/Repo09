2bfeb38473db057a7faca065750a238a5d342a91
Sulejman Catibusic <sulejman.catibusic@gmail.com>
2021-09-22 00:36:26 +0200

    devam ediyoruz

---
+++ ex05/src/index.js
@@ -0,0 +1,10 @@
+function compareStrictValues(m) {
+  // Only change code below this line
+  if (m != 55) {
+    return "Not equal";
+  }
+  return "Equal";
+  // Only change code above this line
+}
+console.log(compareStrictValues(55));
+module.exports = compareStrictValues;
