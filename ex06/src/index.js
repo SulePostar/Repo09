f6f0c564fc5dcee85459628994ae7f1c159a04eb
Sulejman Catibusic <sulejman.catibusic@gmail.com>
2021-09-22 00:47:35 +0200

    devam ediyoruz

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -4,21 +4,21 @@ function comparisonToEqual(a) {
 
     if (a < 5) {
     
-      return "Less than 5";
+      return "Less  than  5";
     }
     
-    return "Less than 10";
+    return "Less   than   10";
   }
   
   if (a >= 10) {
   
     if (a > 20) {
   
-      return "More than 20";
+      return "More    than    20";
   
     }
   
-    return "10 or over";
+    return "10     or     over";
   
   }
   // Only change code above this line
