94de3b191a80ae3a8356f979487618a0e0458172
Sulejman Catibusic <sulejman.catibusic@gmail.com>
2021-09-22 00:13:41 +0200

    devam ediyoruz

---
+++ ex03/src/index.js
@@ -0,0 +1,16 @@
+function myEqualFunction(num) {
+  // Only change code below this line
+
+  if (num === 23) {
+
+    return "Equal";
+
+  }
+
+  return    "Not equal";
+  
+  // Only change code above this line
+}
+
+
+module.exports = myEqualFunction;
