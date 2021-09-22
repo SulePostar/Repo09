c08e2b907f528aa3df7742022c618cc91bbe5815
Sulejman Catibusic <sulejman.catibusic@gmail.com>
2021-09-22 08:46:13 +0200

    devam ediyoruz

---
+++ ex08/src/index08.js
@@ -0,0 +1,10 @@
+function logicalOrOperator(num) {
+  // Only change code below this line
+  if (num < 20 || num > 30) {
+    return "Out";
+  }
+  return "In";
+  // Only change code above this line
+}
+console.log(logicalOrOperator(105));
+module.exports = logicalOrOperator;
