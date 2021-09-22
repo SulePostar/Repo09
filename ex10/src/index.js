9cf1cb5387897ff08d2ebce5ad20d59cb5e1bf33
Sulejman Catibusic <sulejman.catibusic@gmail.com>
2021-09-22 09:24:55 +0200

    devam ediyoruz

---
+++ ex10/src/index.js
@@ -0,0 +1,20 @@
+function logicOrder (num) {
+  // Only change code below this line
+
+  if (num < 50) {
+  
+    return "Less than 50.";
+  
+  } else if (num < 100) {
+  
+    return "Less than 100.";
+  
+  } else {
+  
+    return "Greater than or equal to 100.";
+  }
+
+  // Only change code above this line
+}
+
+module.exports = logicOrder;
