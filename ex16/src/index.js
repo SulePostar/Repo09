8479522674f96c6f7568ba6b961cdc7fac870569
Sulejman Catibusic <sulejman.catibusic@gmail.com>
2021-09-22 10:22:06 +0200

    devam ediyoruz

---
+++ ex16/src/index.js
@@ -0,0 +1,7 @@
+function checkSign(num) {
+  // Only change code below this line
+  return num > 0 ? "positive" : num < 0 ? "negative" : "zero";
+  // Only change code above this line
+}
+console.log(checkSign(0));
+module.exports = checkSign;
