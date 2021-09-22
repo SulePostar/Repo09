b93847103adda8acfe7106a58c5b7b301c6e0f31
Sulejman Catibusic <sulejman.catibusic@gmail.com>
2021-09-22 08:54:46 +0200

    devam ediyoruz

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -5,9 +5,9 @@ function functionElse(num) {
   var result = "";
 
   if (num <= 5) {
-    result = "5  or  smaller";
+    result = "5  or  smaller...";
   } else {
-    result = "Bigger  than     5";
+    result = "Bigger  than     5 !";
   }
 
   return result;
