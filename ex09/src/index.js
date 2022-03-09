0ae389502e6ab473b1a34270de9b225b92aabcd5
Sulejman Catibusic <sulejmanca@maestralsolutions.com>
2022-03-09 17:18:52 +0100

    devam ediyoruz

---
+++ ex09/src/index.js
@@ -0,0 +1,19 @@
+function functionElse(num) {
+
+  // Only change code below this line
+  
+  var result = "";
+
+  if (num <= 5) {
+    result = "5  or  smaller...";
+  } else {
+    result = "Bigger  than     5 !";
+  }
+
+  return result;
+
+  // Only change code above this line
+}
+
+
+module.exports = functionElse;
