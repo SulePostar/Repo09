7ea68762685dd332b25d0263259cdff318453263
Sulejman Catibusic <sulejmanca@maestralsolutions.com>
2022-03-09 17:09:58 +0100

    devam ediyoruz

---
+++ ex08/src/index.js
@@ -0,0 +1,13 @@
+function logicalOrOperator (num) {
+  // Only change code below this line
+  
+  if (num < 20 || num > 30) {
+    return "Out";
+  }
+  return "In";
+  
+  // Only change code above this line
+}
+
+
+module.exports = logicalOrOperator;
