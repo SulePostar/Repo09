f6fc755958c72e29a786b49209846f24fd3c9654
Sulejman Catibusic <sulejmanca@maestralsolutions.com>
2022-03-09 16:21:25 +0100

    devam ediyoruz

---
+++ ex05/src/index.js
@@ -0,0 +1,17 @@
+function compareStrictValues ( m )     {
+  // Only change code below this line
+
+  if (m != 55) {
+  
+    return "Not equal";
+  }
+  
+  return "Equal";
+  
+  
+  
+  // Only change code above this line
+}
+
+
+module.exports = compareStrictValues;
