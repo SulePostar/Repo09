73356619d291d370e99806dd8758d833942f2c35
Sulejman Catibusic <sulejmanca@maestralsolutions.com>
2022-03-09 15:57:22 +0100

    devam ediyoruz

---
+++ ex02/src/index.js
@@ -0,0 +1,13 @@
+function myFunction (myParameter) {
+  // Only change code below this line
+
+  if ( myParameter ) {
+    return "The parameter   is true!";
+  }
+
+  return "The parameter is   false!";
+
+  // Only change code above this line
+}
+
+module.exports =   myFunction;
