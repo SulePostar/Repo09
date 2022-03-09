1ddf38866be6618f89206a413437086d199d0e28
Sulejman Catibusic <sulejmanca@maestralsolutions.com>
2022-03-09 17:16:12 +0100

    devam ediyoruz

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,13 +1,13 @@
-function logicalOrOperator (num) {
+function logicalAndOperator (num) {
   // Only change code below this line
   
-  if (num < 20 || num > 30) {
-    return "Out";
+  if (num < 20 && num > 30) {
+    return "Yes";
   }
-  return "In";
+  return "No";
   
   // Only change code above this line
 }
 
 
-module.exports = logicalOrOperator;
+module.exports = logicalAndOperator;
