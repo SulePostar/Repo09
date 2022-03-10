948ca508a7588c8f8e38b4cbbb6ae845aca99c3c
Sulejman Catibusic <sulejmanca@maestralsolutions.com>
2022-03-10 08:27:19 +0100

    devam ediyoruz

---
+++ ex16/src/index.js
@@ -0,0 +1,27 @@
+function switchCaseSequential(num) {
+  // Only change code below this line
+  let answer = "";
+  switch (num) {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+      break;
+    default:
+      answer = "undefined";
+  }
+  return answer;
+  // Only change code above this line
+}
+
+module.exports = switchCaseSequential;
