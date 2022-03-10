f9afca6c78e9752379ec6a1612de383885e31cd9
Sulejman Catibusic <sulejmanca@maestralsolutions.com>
2022-03-10 08:12:24 +0100

    devam ediyoruz

---
+++ ex14/src/index.js
@@ -0,0 +1,9 @@
+// Only change code below this line
+function checkSign(num) {
+  return (num === 0) ? "zero"
+  : (num < 0) ? "negative"
+  : "positive"
+}
+// Only change code above this line
+
+module.exports = checkSign;
