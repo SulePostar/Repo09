b298d4d5aace71129b32b8eee50822f90a1993da
Sulejman Catibusic <sulejmanca@maestralsolutions.com>
2022-03-10 09:35:16 +0100

    devam ediyoruz

---
+++ ex19/src/index.js
@@ -0,0 +1,14 @@
+// Only change code below this line
+function myDoWhile() {
+  let myNumbers = "";
+  let i = 0;
+  do{
+    myNumbers += i;
+    if(i < 9) myNumbers += ", ";
+    i++;
+  } while (i < 10);
+  return myNumbers;
+}
+// Only change code above this line
+console.log(myDoWhile());
+module.exports = myDoWhile
