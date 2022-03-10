e4e9271680a920bc253193566af50b4ef601dd71
Sulejman Catibusic <sulejmanca@maestralsolutions.com>
2022-03-10 07:56:25 +0100

    devam ediyoruz

---
+++ ex13/src/index.js
@@ -0,0 +1,11 @@
+// Only change code below this line
+function isLess(a, b) {
+  if(a < b) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// Only change code above this line
+
+module.exports = isLess;
